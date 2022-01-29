06f3bdeb5d5de04097a3a7e2d717f25ca6ed31bc
Ginny <ginnynjon@gmail.com>
2022-01-29 15:43:38 +0000

    add flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user.present? && user.authenticate(params[:password])
-      session[:user_id] = user.username
+      session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in Successfully'
     else
       flash 'Invalid email or password'
